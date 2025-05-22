7ce515f9a94c59629296220877ad2b6b6afd1746
cxrlosL15 <160366577+cxrlosL15@users.noreply.github.com>
2025-05-22 02:12:43 -0700

    Menu lateral y horizontal

---
+++ BibliotecaRinconDelLibro/Pages/Shared/_MenuLateral.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Biblioteca.Pages.Shared
+{
+    public class MenuLateralModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
