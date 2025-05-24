34298aceea6abd14a96cc73fe0760d79b711226c
AXEL ENRIQUE NEVARES HERNANDEZ <L22211914@tijuana.tecnm.mx>
2025-05-23 20:20:47 -0700

    Confirmacion

--- BibliotecaRinconDelLibro/Models/BibliotecaContext.cs
+++ BibliotecaRinconDelLibro/Models/BibliotecaContext.cs
@@ -43,7 +43,8 @@ public partial class BibliotecaContext : DbContext
 
     public virtual DbSet<Libro> Libros { get; set; }
 
-    public virtual DbSet<Multa> Multas { get; set; }
+    public DbSet<Multa> Multas { get; set; }
+
 
     public virtual DbSet<Prestamo> Prestamos { get; set; }
 
@@ -64,7 +65,7 @@ public partial class BibliotecaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=BibliotecaRinconDelLibro;Trusted_Connection=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=Elise\\MSSQLSERVER02;Database=BibliotecaRinconDelLibro;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
